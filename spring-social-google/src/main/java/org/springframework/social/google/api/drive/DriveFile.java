8823844448d429446e624c65c843eee5c96885ef
Gabriel Axel <guznik@gmail.com>
2014-05-19 13:38:30 +0300

    Fixed #17

--- spring-social-google/src/main/java/org/springframework/social/google/api/drive/DriveFile.java
+++ spring-social-google/src/main/java/org/springframework/social/google/api/drive/DriveFile.java
@@ -143,7 +143,9 @@ public class DriveFile extends ApiEntity {
 			file.modifiedDate = modifiedDate;
 			file.parents = new ArrayList<DriveFileParent>();
 			for(String parentId : parentIds) {
-				file.parents.add(new DriveFileParent(parentId));
+				if(parentId != null) {
+					file.parents.add(new DriveFileParent(parentId));
+				}
 			}
 			return file;
 		}
