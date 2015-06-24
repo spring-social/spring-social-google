74aa81e37a19195418b2a215323a1d3aa0ec936b
Stephen Kazmierczak <stephen_kazmierczak@percussion.com>
2015-06-24 10:49:45 -0400

    PostActivity now extends Activity so that we can return the newly created Activity PostAttachment now extends Activity.Attachment

--- spring-social-google/src/main/java/org/springframework/social/google/api/plus/posts/PostActivity.java
+++ spring-social-google/src/main/java/org/springframework/social/google/api/plus/posts/PostActivity.java
@@ -1,25 +1,66 @@
 package org.springframework.social.google.api.plus.posts;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.social.google.api.plus.Activity;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+public class PostActivity extends Activity {
 
-public class PostActivity {
-	
 	@JsonProperty
 	private PostObject object;
+
 	@JsonProperty
 	private PostAcl access;
-	public void createActivity(String message){
+
+	@JsonProperty
+	private String verb = "post";
+
+	public PostActivity() {
+	}
+
+	public PostActivity(String message) {
 		object = new PostObject();
-		List<PostAclResource> items = new ArrayList<PostAclResource>();
-		PostAclResource aclResource = new PostAclResource();
-		access = new PostAcl();		
-		aclResource.setType("domain");
-		items.add(aclResource);
 		object.setOriginalContent(message);
+
+		PostAclResource aclResource = new PostAclResource();
+		aclResource.setType("domain");
+
+		List<PostAclResource> items = new ArrayList<PostAclResource>();
+		items.add(aclResource);
+
+		access = new PostAcl();
 		access.setItems(items);
-		access.setDomainRestricted(true);		
+		access.setDomainRestricted(true);
+	}
+
+	@Override
+	public String getContent() {
+		return object.getOriginalContent();
+	}
+
+	@Override
+	public List<Attachment> getAttachments() {
+		if (object.getAttachments() == null){
+			return new ArrayList<Attachment>();
+		} else {
+			return new ArrayList<Attachment>(object.getAttachments());
+		}
+	}
+
+	@Override
+	public int getPlusOners() {
+		return 0;
+	}
+
+	@Override
+	public int getResharers() {
+		return 0;
+	}
+
+	@Override
+	public int getReplies() {
+		return 0;
 	}
 }
